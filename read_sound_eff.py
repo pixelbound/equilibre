2e2cbc7ab3c7e5273f96b9b04e9f894f5bafe8b9
PiB <pixelbound@gmail.com>
2012-08-18 17:23:28 +0100

    Sound.eff file format.

--- read_sound_eff.py
+++ read_sound_eff.py
@@ -12,19 +12,17 @@ def read_sound_eff(f):
         entries.append(entry)
     return entries
 
-if __name__ == "__main__":
-    with open(sys.argv[1], "r") as f:
-        entries = read_sound_eff(f)
+def print_sound_entries(entries):
     for entry in entries:
         formats, fields = [], []
         # flags, field2, field3, entryID
         formats.extend(["%08x", "%d", "%d", "%d"])
         fields.extend(entry[0:4])
         # field8-field11
-        formats.extend(["%d"] * 4)
+        formats.extend(["%08x", "%d", "%d", "%08x"])
         fields.extend(entry[8:12])
         # field12-field15
-        formats.extend(["%04d", "%04d", "%08x", "%04d"])
+        formats.extend(["%04d", "%04d", "%08x", "%08x"])
         fields.extend(entry[12:16])
         # field16-field21
         formats.extend(["%05d", "%05d", "%d", "%04d", "%d"])
@@ -33,3 +31,10 @@ if __name__ == "__main__":
         formats.extend(["%.2f"] * 4)
         fields.extend(entry[4:8])
         print(" ".join([fmt % field for (fmt, field) in zip(formats, fields)]))
+
+if __name__ == "__main__":
+    for file in sys.argv[1:]:
+        with open(file, "r") as f:
+            entries = read_sound_eff(f)
+        print(file)
+        print_sound_entries(entries)
