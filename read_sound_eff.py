6ff5d7133a09cb9c3f991f9d5ec5b3c549612504
PiB <pixelbound@gmail.com>
2012-08-18 15:29:21 +0100

    Sound.eff.

--- read_sound_eff.py
+++ read_sound_eff.py
@@ -1,4 +1,5 @@
 import sys
+import binascii
 import struct
 
 def read_sound_eff(f):
@@ -7,7 +8,7 @@ def read_sound_eff(f):
         data = f.read(84)
         if len(data) < 84:
             break
-        entry = struct.unpack("12sIffff52s", data)
+        entry = struct.unpack("IIIIffff52s", data)
         entries.append(entry)
     return entries
 
@@ -15,4 +16,6 @@ if __name__ == "__main__":
     with open(sys.argv[1], "r") as f:
         entries = read_sound_eff(f)
     for entry in entries:
-        print(repr(entry))
+        flags, field2, field3, entryID, x, y, z, r, field4 = entry
+        print("%08x %d %d %d %s %f %f %f %f" % (flags, field2, field3, entryID, binascii.hexlify(field4), x, y, z, r))
+        #print(binascii.hexlify(field4))
