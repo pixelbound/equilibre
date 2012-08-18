3172a8b0b0de9a31dea20dbc7396648ced9522f4
PiB <pixelbound@gmail.com>
2012-08-18 15:45:16 +0100

    Read sounds.eff

--- read_sound_eff.py
+++ read_sound_eff.py
@@ -8,7 +8,7 @@ def read_sound_eff(f):
         data = f.read(84)
         if len(data) < 84:
             break
-        entry = struct.unpack("IIIIffff52s", data)
+        entry = struct.unpack("4I4f8I20s", data)
         entries.append(entry)
     return entries
 
@@ -16,6 +16,17 @@ if __name__ == "__main__":
     with open(sys.argv[1], "r") as f:
         entries = read_sound_eff(f)
     for entry in entries:
-        flags, field2, field3, entryID, x, y, z, r, field4 = entry
-        print("%08x %d %d %d %s %f %f %f %f" % (flags, field2, field3, entryID, binascii.hexlify(field4), x, y, z, r))
-        #print(binascii.hexlify(field4))
+        formats, fields = [], []
+        # flags, field2, field3, entryID
+        formats.extend(["%08x", "%d", "%d", "%d"])
+        fields.extend(entry[0:4])
+        # field4-field11
+        formats.extend(["%d"] * 4)
+        formats.extend(["%04d"] * 2)
+        formats.extend(["%08x"] * 1)
+        formats.extend(["%04d"] * 1)
+        fields.extend(entry[8:16])
+        # x, y, z, r
+        formats.extend(["%f"] * 4)
+        fields.extend(entry[4:8])
+        print(" ".join([fmt % field for (fmt, field) in zip(formats, fields)]))
