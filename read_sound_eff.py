43a868d4c3366a0d326d98856c0e6176afc524cb
PiB <pixelbound@gmail.com>
2012-08-18 19:22:33 +0100

    Sounds and Music.

--- read_sound_eff.py
+++ read_sound_eff.py
@@ -8,7 +8,7 @@ def read_sound_eff(f):
         data = f.read(84)
         if len(data) < 84:
             break
-        entry = struct.unpack("4I4f13I", data)
+        entry = struct.unpack("4I4f6I2H6I", data)
         entries.append(entry)
     return entries
 
@@ -16,17 +16,17 @@ def print_sound_entries(entries):
     for entry in entries:
         formats, fields = [], []
         # flags, field2, field3, entryID
-        formats.extend(["%08x", "%d", "%d", "%d"])
+        formats.extend(["0x%08x", "%d", "%d", "%d"])
         fields.extend(entry[0:4])
         # field8-field11
-        formats.extend(["%08x", "%d", "%d", "%08x"])
+        formats.extend(["0x%08x", "%d", "%d", "0x%08x"])
         fields.extend(entry[8:12])
-        # field12-field15
-        formats.extend(["%04d", "%04d", "%08x", "%08x"])
-        fields.extend(entry[12:16])
-        # field16-field21
+        # field12-field16
+        formats.extend(["%04d", "%04d", "0x%04x", "0x%04x", "0x%08x"])
+        fields.extend(entry[12:17])
+        # field17-field21
         formats.extend(["%05d", "%05d", "%d", "%04d", "%d"])
-        fields.extend(entry[16:21])
+        fields.extend(entry[17:22])
         # x, y, z, r
         formats.extend(["%.2f"] * 4)
         fields.extend(entry[4:8])
