12e430c15ca61d4f51f7f18d01258f4f6b14f917
PiB <pixelbound@gmail.com>
2013-04-13 22:49:30 +0100

    Send login request to world server.

--- login.py
+++ login.py
@@ -2,6 +2,8 @@ import socket
 import struct
 import collections
 import binascii
+import zlib
+import time
 import sys
 import argparse
 
@@ -10,7 +12,7 @@ SM_SessionRequest = 0x01
 SM_SessionResponse = 0x02
 SM_Combined = 0x03
 SM_SessionDisconnect = 0x05
-SM_LoginPacket = 0x09
+SM_ApplicationPacket = 0x09
 SM_Ack = 0x15
 SM_Types = {value: name for name, value in globals().items() if name.startswith("SM_")}
 
@@ -25,6 +27,12 @@ LM_LoginResponse = 0x17
 LM_ServerListResponse = 0x18
 LM_PlayResponse = 0x21
 LM_Types = {value: name for name, value in globals().items() if name.startswith("LM_")}
+
+# World message types (Titanium).
+WM_SendLoginInfo = 0x4dd0
+WM_Types = {value: name for name, value in globals().items() if name.startswith("WM_")}
+
+ALL_Types = {"SM": SM_Types, "LM": LM_Types, "WM": WM_Types}
 
 # Possible status values in PlayResponse messages.
 PLAY_Allowed = 101
@@ -49,7 +57,7 @@ class Message(object):
     def add_param(self, name, code, value=None):
         self.params[name] = Parameter(name, code, value)
     
-    def serialize(self, crc32_fn=None):
+    def serialize(self):
         patterns = []
         if self.ns == "SM":
             patterns.append("!")
@@ -63,9 +71,6 @@ class Message(object):
         data = struct.pack("".join(patterns), *args)
         if self.body:
             data += self.body
-        # Add checksum at the end of the packet.
-        if (self.ns == "SM") and (self.type not in (SM_SessionRequest, SM_SessionResponse)):
-            data += struct.pack("H", crc32_fn(data))
         return data
     
     def deserialize(self, data):
@@ -106,16 +111,11 @@ class Message(object):
 
     def __str__(self):
         chunks = []
-        if self.ns == "SM":
-            try:
-                msg_name = SM_Types[self.type]
-            except KeyError:
-                msg_name = "SessionMessage_%02x" % self.type
-        elif self.ns == "LM":
-            try:
-                msg_name = LM_Types[self.type]
-            except KeyError:
-                msg_name = "LoginMessage_%02x" % self.type
+        msg_types = ALL_Types[self.ns]
+        try:
+            msg_name = msg_types[self.type]
+        except KeyError:
+            msg_name = "%s_%02x" % (self.__class__.__name__, self.type)
             
         param_chunks = []
         for param in self.params.values():
@@ -134,10 +134,15 @@ class LoginMessage(Message):
     def __init__(self, type):
         super(LoginMessage, self).__init__("LM", type)
 
+class WorldMessage(Message):
+    def __init__(self, type):
+        super(LoginMessage, self).__init__("WM", type)
+
 class SessionClient(object):
     def __init__(self, addr):
         self.addr = addr
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.compressed = False
         self.crc_key = 0
         self.next_ack_in = 0
         self.next_seq_in = 0
@@ -188,9 +193,22 @@ class SessionClient(object):
         elif self._has_seq_num(msg.type):
             msg.add_param("SeqNum", "H", self.next_seq_out)
             self.next_seq_out += 1
-        #XXX Figure out why we need to byte-swap the checksum before sending it.
-        crc32_fn = lambda data: socket.htons(self._crc32(data) & 0xffff)
-        self._send_packet(msg.serialize(crc32_fn))
+        packet = msg.serialize()
+        
+        # Compress the packet if asked to and if it makes sense.
+        if self.compressed:
+            header, payload = packet[0:2], packet[2:]
+            compression_flag = chr(0xa5) # Uncompressed by default.
+            if len(payload) > 10:
+                payload, compression_flag = zlib.compress(payload), 'Z'
+            packet = header + compression_flag + payload
+        
+        # Add checksum at the end of the packet.
+        # XXX Figure out why we need to byte-swap it before sending.
+        if msg.type not in (SM_SessionRequest, SM_SessionResponse):
+            crc = socket.htons(self._crc32(packet) & 0xffff)
+            packet += struct.pack("H", crc)
+        self._send_packet(packet)
     
     def _format_addr(self, addr):
         return "%s:%d" % addr
@@ -225,7 +243,7 @@ class SessionClient(object):
     def _has_seq_num(self, sm_type):
         """ Determine whether the message has a sequence number parameter and
         whether it should be incremented before sending it. """
-        return sm_type in (SM_LoginPacket, )
+        return sm_type in (SM_ApplicationPacket, )
 
     def _crc32_round(self, a, b):
         return (a >> 8) ^ CRC32Lookup[(b ^ a) & 0xff]
@@ -249,6 +267,20 @@ class SessionClient(object):
                 raise ValueError("Invalid CRC: computed 0x%04x, but found 0x%04x." % (computed_crc, crc))
         packet = packet[2:]
         
+        # Decompresse the packet's payload if needed.
+        if self.compressed:
+            # Check the flag byte to see if the packet really is compressed.
+            compression_flag = packet[0]
+            packet = packet[1:]
+            if compression_flag == 'Z':
+                # Deflate-compressed.
+                packet = zlib.decompress(packet)
+            elif compression_flag == chr(0xa5):
+                # Not compressed.
+                pass
+            else:
+                raise ValueError("Invalid compression flag: 0x%x." % ord(compression_flag))
+        
         # Specify the header fields for the message, if we know about any.
         msg = SessionMessage(msg_type)
         if msg_type == SM_SessionResponse:
@@ -259,7 +291,7 @@ class SessionClient(object):
             msg.add_param("UnknownB", "B")
             msg.add_param("MaxLength", "I")
             msg.add_param("UnknownC", "I")
-        elif msg_type == SM_LoginPacket:
+        elif msg_type == SM_ApplicationPacket:
             msg.add_param("SeqNum", "H")
         elif msg_type == SM_Ack:
             msg.add_param("SeqNum", "H")
@@ -270,9 +302,11 @@ class SessionClient(object):
 
 class ApplicationClient(object):
     """ High-level interface to send and receive messages over a session. """
-    def __init__(self):
+    def __init__(self, msg_types, compressed):
+        self.msg_types = msg_types
         self.session_id = 0x26ec5075 # XXX Should it be random?
         self.session = None
+        self.compressed = compressed
     
     def __enter__(self):
         pass
@@ -283,7 +317,7 @@ class ApplicationClient(object):
     def connect(self, remote_addr):
         """ Initiate a session with the remote server. """
         if self.session:
-            pass
+            return
         session = SessionClient(remote_addr)
         connected = False
         try:
@@ -300,11 +334,12 @@ class ApplicationClient(object):
                 raise Exception("Server responded with different session ID: 0x%x, ours: 0x%x"
                     % (response_id, self.session_id))
             session.crc_key = response["Key"]
+            session.compressed = self.compressed
+            self.session = session
             connected = True
         finally:
             if not connected:
                 session.close()
-        self.session = session
     
     def disconnect(self):
         if not self.session:
@@ -319,23 +354,23 @@ class ApplicationClient(object):
             self.session = None
     
     def receive(self):
-        """ Wait until a message has been received from the server. """
+        """ Wait until an application message has been received from the server. """
         session_msg = self.session.receive()
         if (not session_msg) or (session_msg.type == SM_SessionDisconnect):
             self.active = False
             return None
-        elif session_msg.type == SM_LoginPacket:
+        elif session_msg.type == SM_ApplicationPacket:
             return self._parse_packet(session_msg.body)
         else:
             print("Unexpected session message: %s" % session_msg)
             return None
 
-    def send(self, login_msg):
-        """ Send a login message to the server. """
-        if login_msg.ns != "LM":
-            raise ValueError("Not a login message.")
-        session_msg = SessionMessage(SM_LoginPacket)
-        session_msg.body = login_msg.serialize()
+    def send(self, app_msg):
+        """ Send an application message to the server. """
+        if app_msg.ns not in ("LM", "WM"):
+            raise ValueError("Not an application message.")
+        session_msg = SessionMessage(SM_ApplicationPacket)
+        session_msg.body = app_msg.serialize()
         self.session.send(session_msg)
 
     def _parse_packet(self, packet):
@@ -346,7 +381,7 @@ class ApplicationClient(object):
         
         # Call the function that can parse the message, if it exists.
         try:
-            msg_name = LM_Types[msg_type]
+            msg_name = self.msg_types[msg_type]
         except KeyError:
             pass
         else:
@@ -367,7 +402,7 @@ class ApplicationClient(object):
 class LoginClient(ApplicationClient):
     """ High-level interface to talk to a login server. """
     def __init__(self):
-        super(LoginClient, self).__init__()
+        super(LoginClient, self).__init__(LM_Types, False)
     
     def begin_get_chat_message(self):
         request = LoginMessage(LM_ChatMessageRequest)
@@ -477,6 +512,27 @@ class LoginClient(ApplicationClient):
         msg.add_param("ServerID", "I")
         msg.deserialize(packet)
 
+class WorldClient(ApplicationClient):
+    """ High-level interface to talk to a world server. """
+    def __init__(self):
+        super(WorldClient, self).__init__(WM_Types, True)
+    
+    def begin_login(self, server_id, session_key):
+        zoning = "\x00" # "\x01" when zoning.
+        request = LoginMessage(WM_SendLoginInfo)
+        body_chunks = []
+        body_chunks.append(str(server_id))
+        body_chunks.append("\x00")
+        body_chunks.append(session_key)
+        body_chunks.append("\x00")
+        padding = 124 + 64 - sum(len(chunk) for chunk in body_chunks)
+        body_chunks.append("\x00" * padding)
+        body_chunks.append(zoning)
+        padding2 = 464 - sum(len(chunk) for chunk in body_chunks)
+        body_chunks.append("\x00" * padding2)
+        request.body = "".join(body_chunks)
+        self.send(request)
+
 class ServerInfo(object):
     def __init__(self):
         self.type = 0
@@ -492,6 +548,8 @@ def client_login(server_addr, username, password):
     client.connect(server_addr)
     with client:
         stage = 0
+        server_by_id = {}
+        session_key = None
         client.begin_get_chat_message()
         response = client.receive()
         while response:
@@ -518,6 +576,7 @@ def client_login(server_addr, username, password):
                 servers = client.end_list_servers(response)
                 print("%d servers online." % len(servers))
                 for i, server in enumerate(servers):
+                    server_by_id[server.runtime_id] = server
                     print("%d: %s (%d players, ID=%d)" % (i, server.name,
                         server.players, server.runtime_id))
                 # Login in the first server in the list.
@@ -533,11 +592,37 @@ def client_login(server_addr, username, password):
                 # Waiting for a play response.
                 handled = True
                 success, status, server_id, sequence = client.end_play(response)
+                server = server_by_id[server_id]
                 if success:
-                    print("Server %d allowed play." % server_id)
+                    print("Server '%s' (ID=%d) allowed play." % (server.name, server_id))
+                    # HACK: give the login server a chance to tell the world
+                    # server that we can log in.
+                    time.sleep(0.1)
+                    client_play(server, sequence, session_key)
                 else:
-                    print("Server %d disallowed play, reason = %d." % (server_id, status))
+                    print("Server '%s' (ID=%d) disallowed play, reason = %d." %
+                        (server.name, server_id, status))
                     break
+            if not handled:
+                print(response)
+            response = client.receive()
+
+def client_play(server, sequence, session_key):
+    client = WorldClient()
+    server_addr = (server.host, 9000)
+    client.connect(server_addr)
+    with client:
+        stage = 0
+        client.begin_login(sequence, session_key)
+        response = client.receive()
+        while response:
+            handled = False
+            #if (stage == 0) and (response.type == LM_ChatMessageResponse):
+            #    # Waiting for a login response.
+            #    print("Chat message: %s" % client.end_get_chat_message(response))
+            #    stage = 1
+            #    handled = True
+            #    client.begin_login(username, password)
             if not handled:
                 print(response)
             response = client.receive()
